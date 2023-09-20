0bb0a7afc79eacb0038e4433ab8ba9c8a20ccec3
XiaoBaiiiiii <bcg971004@icloud.com>
2023-09-20 19:23:20 +0800

    second commit

--- pcd_proj_img.cc
+++ pcd_proj_img.cc
@@ -1,12 +1,17 @@
 #include <iostream>
 #include <yaml-cpp/yaml.h>
+#include "opencv2/opencv.hpp"
+#include "opencv2/highgui.hpp"
+#include "opencv2/imgproc/imgproc.hpp"
 
 #include "common_lib.h"
 
 // P_i = extrin_R_ * P_L +  extrin_t_;
-Eigen::Matrix3d extrin_R_;
-Eigen::Vector3d extrin_t_; 
+Eigen::Matrix3f extrin_R_;
+Eigen::Vector3f extrin_t_; 
+pcl::PointCloud<pcl::PointXYZ>::Ptr pcd_ptr_;
 
+bool readPCD(std::string&)
 int main(int argc, char** argv) {
   if (argc != 2) {
     std::cerr << "Usage: ./pcd_proj_img <yaml_file_path>" << std::endl;
@@ -22,10 +27,17 @@ int main(int argc, char** argv) {
   extrin_t = config["extrinsic_t"].as<std::vector<double>>();
   extrin_R = config["extrinsic_R"].as<std::vector<double>>();
 
-  extrin_t_ = common::VecFromArray<double>(extrinT_);
-  extrin_R_ = common::MatFromArray<double>(extrinR_);
+  extrin_t_ = common::VecFromArray<float>(extrinT_);
+  extrin_R_ = common::MatFromArray<float>(extrinR_);
 
-  
+  std::string folder_path = pcd_file.substr(0, pcd_file.find_last_of('/'));
+  std::string proj_img_path = folder_path + "/proj.png";
+
+  cv::Mat original_image = cv::imread(img_file);
+
+  // 读取点云
+
+
 
 
 }
